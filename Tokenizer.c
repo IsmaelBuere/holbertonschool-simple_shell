058a2a1962c638522a751abc9f6f94fd591f36c5
IsmaelBuere <146840416+IsmaelBuere@users.noreply.github.com>
2023-12-16 12:03:20 -0300

    Tokenizer fix

--- Tokenizer.c
+++ Tokenizer.c
@@ -8,40 +8,52 @@
  */
 char **split_string(char *buffer, char *del)
 {
-	char **tokens = NULL;
-	char *token;
-	int j, i = 0;
+    char **tokens = NULL;
+    char *token;
+    int j, i = 0;
 
-	token = strtok(buffer, del);
-	while (token)
-	{
-		char **new_tokens = realloc(tokens, sizeof(char *) * (i + 2));
-		if (!new_tokens)
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(tokens[j]);
-			}
-			free(tokens);
-			return (NULL);
-		}
-		tokens = new_tokens;
-		tokens[i] = strdup(token);
-		if (!tokens[i])
-		{
-			for (j = 0; j < i; j++)
-			{
-				free(tokens[j]);
-			}
-			free(tokens);
-			return (NULL);
-		}
+    token = strtok(buffer, del);
+    while (token)
+    {
+        char **new_tokens = realloc(tokens, sizeof(char *) * (i + 2));
+        if (!new_tokens)
+        {
+            for (j = 0; j < i; j++)
+            {
+                free(tokens[j]);
+            }
+            free(tokens);
+            return (NULL);
+        }
+        tokens = new_tokens;
 
-		token = strtok(NULL, del);
-		i++;
-	}
+        tokens[i] = strdup(token);
+        if (!tokens[i])
+        {
+            for (j = 0; j < i; j++)
+            {
+                free(tokens[j]);
+            }
+            free(tokens);
+            return (NULL);
+        }
 
-	tokens[i] = NULL;
-	return (tokens);
+        token = strtok(NULL, del);
+        i++;
+    }
+
+    char **final_tokens = realloc(tokens, sizeof(char *) * (i + 1));
+    if (!final_tokens)
+    {
+        for (j = 0; j < i; j++)
+        {
+            free(tokens[j]);
+        }
+        free(tokens);
+        return (NULL);
+    }
+    tokens = final_tokens;
+    tokens[i] = NULL;
+
+    return (tokens);
 }
-
