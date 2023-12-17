efedf31d62f9db0b46e4d02a4f814e84e4dd77e6
Eithan-Tabarez <146757670+Eithan-Tabarez@users.noreply.github.com>
2023-12-17 13:43:21 -0300

    Shell

--- shell.h
+++ shell.h
@@ -12,7 +12,7 @@ extern char **environ;
 char *_getenv(const char *env_var);
 char *get_command(char *command);
 char **split_string(char *buffer, char *del);
-void free_tokens(char **tokens);
+int free_tokens(char **tokens);
 void execute_command(char **args, char **env);
 int main(int ac, char **av, char **env);
 int is_terminal(int fd);
